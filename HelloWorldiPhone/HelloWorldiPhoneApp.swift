c87fabac7a891f72616c5053cdf4be3819c308a0
matt.a@myplanet.com <matt.a@myplanet.com>
2023-11-17 22:38:17 -0500

    Initial Commit

---
+++ HelloWorldiPhone/HelloWorldiPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldiPhoneApp.swift
+//  HelloWorldiPhone
+//
+//  Created by Matt Ahmad on 2023-11-17.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldiPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
